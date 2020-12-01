21b3b3ecdefde5ddd90270b7b1afeb5034bcf8ec
Enzo <enzoxz.023@gmail.com>
2020-12-01 11:55:09 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3005;
+
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log(`App runing on ${port}`);
+});
