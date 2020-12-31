ae49b8d2b632934c421f84e0000ea432d9430272
Varela69 <60364575+Varela69@users.noreply.github.com>
2020-12-30 23:52:25 -0300

    Agregado trigger para update y delete de ingresos y salidas

--- SQL BASE DE DATOS.sql
+++ SQL BASE DE DATOS.sql
@@ -106,6 +106,28 @@ CREATE TRIGGER retiro
 	AFTER INSERT ON salida
     FOR EACH ROW
 		UPDATE producto SET stock = stock - NEW.cantidad WHERE id = NEW.id_producto;
+		
+CREATE TRIGGER cambio_ingreso
+	AFTER UPDATE ON entrada
+    FOR EACH ROW
+		UPDATE producto SET stock = stock - OLD.cantidad WHERE id = NEW.id_producto;
+		UPDATE producto SET stock = stock + NEW.cantidad WHERE id = NEW.id_producto;
+
+CREATE TRIGGER cambio_retiro
+	AFTER UPDATE ON salida
+    FOR EACH ROW
+		UPDATE producto SET stock = stock + OLD.cantidad WHERE id = NEW.id_producto;
+		UPDATE producto SET stock = stock - NEW.cantidad WHERE id = NEW.id_producto;
+
+CREATE TRIGGER eliminar_ingreso
+	AFTER DELETE ON entrada
+    FOR EACH ROW
+		UPDATE producto SET stock = stock - OLD.cantidad WHERE id = NEW.id_producto;
+
+CREATE TRIGGER eliminar_retiro
+	AFTER DELETE ON salida
+    FOR EACH ROW
+		UPDATE producto SET stock = stock + OLD.cantidad WHERE id = NEW.id_producto;
         
 INSERT INTO categoria (nombre) VALUES ("Electronica");
 INSERT INTO subcategoria (nombre, id_categoria) VALUES ("Laptop", 1);
