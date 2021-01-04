a0a7529cdffe0782d35f1104d76bd55a7ebfa753
Enzo <enzoxz.023@gmail.com>
2021-01-04 15:27:16 -0300

    Cambio puerto por defecto

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const categoriesRouter = require("./src/routes/categorias");
 const subcategoriesRouter = require("./src/routes/subcategorias");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3005;
 
 app.use(bodyParser.json());
 app.use(cors());
